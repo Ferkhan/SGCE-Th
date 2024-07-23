ff63eef45115e143f7e1d267cb2e4fe61d5010b1
Ferkhan <133190285+Ferkhan@users.noreply.github.com>
2024-07-22 19:53:25 -0500

    cambio

--- src/app/borrarrrrr.java
+++ src/app/borrarrrrr.java
@@ -3,6 +3,7 @@ package app;
 public class borrarrrrr {
     void claseTrucha() {
         System.out.println("Esta es una clase trucha");
+        System.out.println("Se ha realizado un cambio");
     }
 }
 
